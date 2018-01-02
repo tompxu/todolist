14bb9e8569059ac999d46b9f863431e0b9347217
Tom Xu <pengxu.tom@gmail.com>
2018-01-01 23:35:15 -0500

    django todolist app initiated

---
+++ todos/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^details/(?P<id>\w{0,50})/$', views.details),
+    url(r'^add/(?P<id>\w{0,50})/$', views.add)
+]
---
+++ todos/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Todo
+# Create your views here.
+def index(request):
+	todos = Todo.objects.all()[:10]
+	context = {
+		'todos': todos
+	}
+	return render(request, 'index.html', context)
+
+def details(request, id):
+	todo = Todo.objects.get(id=id)
+	context = {
+		'todo': todo
+	}
+	return render(request, 'details.html', context)
